a3e2d7c36c0ad3902470363a16a0bcc188d28766
VesqueCédric <cedricvesque@gmail.com>
2023-07-17 12:46:00 +0200

    update header

--- src/components/App/Header/Header.tsx
+++ src/components/App/Header/Header.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 
+import './Header.scss';
+
+import { Menu, Person } from '@mui/icons-material';
 
 function Header() {
   return (
-    <>
-      <h2>Header</h2>
-    </>
+    <header>
+      <div className="header-icons">
+        <Menu fontSize="large" />
+      </div>
+      <h1>O'Invoice</h1>
+      <div className="header-icons">
+        <Person fontSize="large" />
+      </div>
+    </header>
   );
 }
 
