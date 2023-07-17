4f7c7217270f76ad2d90cbcab130458eec8e2ece
VesqueCédric <cedricvesque@gmail.com>
2023-07-17 14:09:48 +0200

    add homme component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
+import Home from './Home/Home'
 import './App.scss';
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
       <Footer />
     </>
   );
