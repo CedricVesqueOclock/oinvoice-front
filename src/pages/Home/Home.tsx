01eb2bf029e5400a35c888f7f3d115e0744988fd
VesqueCédric <cedricvesque@gmail.com>
2023-08-01 15:17:27 +0200

    update Dashboard.tsx & Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -11,18 +11,15 @@ function Home() {
   return (
     <>
       <Header />
-      <div className="home">
-        <img className="home-img" src="" alt="" />
-        <p className="home-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos pariatur
-          quo sunt delectus, laudantium vero voluptas veniam amet similique,
-          assumenda aut odio corrupti debitis reprehenderit, ex et eos possimus
-          asperiores tenetur commodi quae mollitia esse? Quibusdam perferendis
-          vel similique adipisci nihil quisquam perspiciatis fuga reprehenderit
-          a, ut impedit, tempora esse?
-        </p>
-        <img className="home-img" src="" alt="" />
-      </div>
+      <div className="home" />
+      <p className="home-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos pariatur
+        quo sunt delectus, laudantium vero voluptas veniam amet similique,
+        assumenda aut odio corrupti debitis reprehenderit, ex et eos possimus
+        asperiores tenetur commodi quae mollitia esse? Quibusdam perferendis vel
+        similique adipisci nihil quisquam perspiciatis fuga reprehenderit a, ut
+        impedit, tempora esse?
+      </p>
       <Footer />
     </>
   );
